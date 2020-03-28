c60617c0427c3bf605949b4b79baef462b35ffc3
Fahim Ahmed <fahim152@gmail.com>
2020-03-28 16:58:44 +0600

    DB Migration file minor issue

--- database/migrations/2020_03_28_104801_create_todo_lists_table.php
+++ database/migrations/2020_03_28_104801_create_todo_lists_table.php
@@ -28,6 +28,6 @@ class CreateTodoListsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todo_list');
+        Schema::dropIfExists('todo_lists');
     }
 }
